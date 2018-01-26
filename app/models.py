4801f4eb28754db1e0b01092bb12daf905272b1b
kratorr <31060213+kratorr@users.noreply.github.com>
2018-01-26 17:54:56 +0300

    add avatars

--- app/models.py
+++ app/models.py
@@ -11,6 +11,7 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    avatar = db.Column(db.String(120),index=True, unique=True)
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
--- app/routes.py
+++ app/routes.py
@@ -61,12 +61,13 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/user/<username>')
 @login_required
 def user(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = [
-        {'author':user, 'bodt': 'test post1'},
+        {'author': user, 'bodt': 'test post1'},
         {'author': user, 'body': 'Test post #2'}
     ]
     return render_template('user.html', user=user, posts=posts)
